ccfee28f259a057388701869730f6070c1c03fb9
DeveloperYamin <developer.yamin@gmail.com>
2022-06-02 00:10:39 +0600

    Screen Cleanup

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+
+export default function HomeScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
