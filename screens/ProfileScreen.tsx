a68e1427d4e70ee6701f53e2d872798f710bd7f7
DeveloperYamin <developer.yamin@gmail.com>
2022-06-04 17:42:59 +0600

    all done!!

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,15 +1,59 @@
 import { Entypo, Feather } from "@expo/vector-icons";
-import { useSignOut } from "@nhost/react";
-import React from "react";
-import { Image, ScrollView, StyleSheet, useColorScheme } from "react-native";
+import { useNhostClient, useSignOut, useUserId } from "@nhost/react";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  ScrollView,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
 import pins from "../assets/data/pins";
 import MasonryList from "../components/MasonryList";
 import { Text, View } from "../components/Themed";
+
+const GET_USER_QUERY = `
+query MyQuery($id: uuid!) {
+  user(id: $id) {
+    id
+    avatarUrl
+    displayName
+    pins {
+      id
+      image
+      title
+      created_at
+    }
+  }
+}
+`;
 
 export default function ProfileScreen() {
   const colorScheme = useColorScheme();
   const isDarkMode = colorScheme === "dark";
   const { signOut } = useSignOut();
+  const nhost = useNhostClient();
+  const [user, setUser] = useState();
+  const userId = useUserId();
+
+  const fetchUserData = async () => {
+    const result = await nhost.graphql.request(GET_USER_QUERY, { id: userId });
+    console.log(result);
+    if (result.error) {
+      Alert.alert("Error fetching the user");
+    } else {
+      setUser(result.data.user);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  if (!user) {
+    return <ActivityIndicator />;
+  }
 
   return (
     <ScrollView style={{ flex: 1 }}>
@@ -31,18 +75,18 @@ export default function ProfileScreen() {
         </View>
         <Image
           source={{
-            uri: "https://scontent.fdac135-1.fna.fbcdn.net/v/t1.6435-9/148289406_105993934859783_4291012364172084677_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHHignath6-ofch3atPiqpE64w4ust0LsTrjDi6y3QuxCkntvw6oUkXov-djo-6EvaEanD96ck_s99Yqu9urpyh&_nc_ohc=EHY6wbmB4vEAX-zQaxg&_nc_ht=scontent.fdac135-1.fna&oh=00_AT9VhOYysxQoQ7qlxtVsf4-kFwsmiVv88-pGYgGjgkjAhw&oe=62BED03D",
+            uri: user.avatarUrl,
           }}
           style={styles.image}
         />
-        <Text style={styles.title}>Yamin</Text>
+        <Text style={styles.title}>{user.displayName}</Text>
         <Text
           style={{ ...styles.subtitle, color: isDarkMode ? "white" : "black" }}
         >
-          123 Follower | 456 Followings
+          123 Follower | 543 Followings
         </Text>
       </View>
-      <MasonryList pins={pins} />
+      <MasonryList pins={user.pins} onRefresh={fetchUserData} />
     </ScrollView>
   );
 }
